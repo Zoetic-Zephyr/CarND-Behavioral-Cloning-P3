7ef59ea2589757222bae85f1cf24255948abdcbd
Zoetic-Zephyr <31701275+Zoetic-Zephyr@users.noreply.github.com>
2019-09-19 23:41:36 +0800

    Adopt NVIDIA's end-to-end CNN, achieving excellent performance on default track using provided training data

--- model.py
+++ model.py
@@ -4,8 +4,8 @@ import numpy as np
 
 lines = []
 
-with open("../../../opt/carnd_p3/data/driving_log.csv") as csvfile:
-# with open("../data/driving_log.csv") as csvfile:
+# with open("../../../opt/carnd_p3/data/driving_log.csv") as csvfile:
+with open("../data/driving_log.csv") as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         if line[0] != "center":
@@ -23,8 +23,8 @@ for line in lines:
     for i in range(3):
         source_path = line[i]
         filename = line[i].split('/')[-1]
-        current_path = '../../../opt/carnd_p3/data/IMG/' + filename
-        # current_path = '../data/IMG/' + filename
+        # current_path = '../../../opt/carnd_p3/data/IMG/' + filename
+        current_path = '../data/IMG/' + filename
         image = ndimage.imread(current_path)
         images.append(image)
 
@@ -50,18 +50,26 @@ X_train = np.array(augumented_images)
 y_train = np.array(augumented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D
-
+from keras.layers import Flatten, Dense, Lambda, Conv2D
 from keras.layers import Cropping2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
+model.add(Conv2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Conv2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Conv2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Conv2D(64,3,3,activation="relu"))
+model.add(Conv2D(64,3,3,activation="relu"))
+
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
 
 model.save('model.h5')
 
